b4a9091537e9253e48fd6a0576a97bc0d015b6a0
lifanova <veralifanovaa@gmail.com>
2021-03-06 17:19:59 +0300

    Report.md

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         double regularBonus = 0.3;
-        double specialBonus = 0.6;
+        double specialBonus = -0.2;
         double totalBonus = regularBonus + specialBonus;
         System.out.println(totalBonus);
     }
